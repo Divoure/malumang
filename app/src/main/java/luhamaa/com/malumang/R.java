68e839347b6414e0a9b15fc9c455c3e9228765ad
Opilane TTHK <opilane@tthk.ee>
2018-02-07 16:22:13 +0200

    Initial commit

---
+++ app/src/main/java/luhamaa/com/malumang/R.java
@@ -0,0 +1,8 @@
+package luhamaa.com.malumang;
+
+/**
+ * Created by opilane on 07.02.2018.
+ */
+
+class R {
+}
---
+++ app/src/main/java/luhamaa/com/malumang/StartActivity.java
@@ -0,0 +1,8 @@
+package luhamaa.com.malumang;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+class StartActivity {
+}
